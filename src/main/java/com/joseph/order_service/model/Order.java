36c28fe9aed5f3a219f5e78b48d4c59a3b8e1583
Titous10 <jjtoussaint23@gmail.com>
2025-07-11 23:17:11 -0400

    adding correct URL change DB name from order to orders

--- src/main/java/com/joseph/order_service/model/Order.java
+++ src/main/java/com/joseph/order_service/model/Order.java
@@ -3,10 +3,12 @@ package com.joseph.order_service.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
+@Table(name = "orders")  // plural or another name
 @Setter
 @Getter
 public class Order {
