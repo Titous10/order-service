7dbd943852bb47c02b3f43d9f1e27ec3b635f443
Titous10 <jjtoussaint23@gmail.com>
2026-02-21 17:11:29 -0500

    Prevent Message Loss (Manual Commit)

--- src/main/java/com/joseph/order_service/kafka/PaymentConsumer.java
+++ src/main/java/com/joseph/order_service/kafka/PaymentConsumer.java
@@ -1,16 +1,21 @@
 package com.joseph.order_service.kafka;
 
 import com.joseph.order_service.model.OrderEvent;
+import org.apache.kafka.clients.consumer.internals.Acknowledgements;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PaymentConsumer {
     @KafkaListener(topics = "orders", groupId = "payment-group")
-    public void consume(OrderEvent event) throws InterruptedException {
+    public void consume(OrderEvent event, Acknowledgment ack) throws InterruptedException {
+        processPayment(event);
+        ack.acknowledge(); // commit ONLY after success
+    }
 
+    private void processPayment(OrderEvent event) throws InterruptedException{
         System.out.println("Processing payment: " + event.orderId());
-
         Thread.sleep(2000); // simulate real processing
     }
 }
